57a06176828a84179164787cc4348fe6f75e1edd
Kalin <nojsullivan@gmail.com>
2026-05-27 18:00:45 -0700

    will remove installs soon in favor of regular shit

--- NojOS.js
+++ NojOS.js
@@ -1,4 +1,3 @@
-import {sinker} from './installs/sinker.js'
 /*
 The following is a work made with the help of many online resources,
 Every website and person sourced in this project will be listed in what
